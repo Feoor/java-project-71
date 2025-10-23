2f18e0fbe0c74a998b3efa261af1f096b6f90795
Feor <mistilplay558@gmail.com>
2025-10-23 15:56:40 +0500

    Implement command-line interface using Picocli and add Differ class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true)
+public class App implements Runnable {
+    @Override
+    public void run() {
+        // business logic here
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute("--help");
+        System.exit(exitCode);
     }
 }
