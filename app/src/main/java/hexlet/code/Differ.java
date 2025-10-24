c7409f26d74018e0ffe70633c899fe7733cb5fcb
Feor <mistilplay558@gmail.com>
2025-10-24 16:38:23 +0500

    Implement file reading and initial structure for Differ class; add test class and sample JSON files

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,26 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
 public class Differ {
+
+//    public static Path getPath(String path) {
+//        return Path.of(path);
+//    }
+
+    public static String getContent(String path) throws Exception {
+        Path filePath = Path.of(path);
+        return Files.readString(filePath);
+    }
+
+    public static Map getData(String content) throws Exception {
+        return null;
+    }
+
+    public static String diff(Map data1, Map data2) {
+        return null;
+    }
 
 }
---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DifferTest {
+
+    @Test
+    void testDiff() {
+
+    }
+
+}
