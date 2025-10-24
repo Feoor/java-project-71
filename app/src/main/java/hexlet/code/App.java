b48737f815ad2d777b7c3186af8fe07c407b1d75
Feor <mistilplay558@gmail.com>
2025-10-24 16:38:12 +0500

    Add options for output format and file paths in App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,9 +7,20 @@ import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true)
 public class App implements Runnable {
+
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+    String format = "stylish";
+
+    @Parameters(index = "0", description = "path to first file")
+    private String file1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String file2;
+
     @Override
     public void run() {
-        // business logic here
+        System.out.println("Formatting with " + format + " format");
+        System.out.println("Comparing '" + file1 + "' with '" + file2 + "'");
     }
 
     public static void main(String[] args) {
